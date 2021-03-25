332f8491fd699637d0d49c4f2dc6cdad302922c2
CatalystJesal <shadowii.jp@gmail.com>
2021-03-25 01:15:54 +0000

    Login screen authentication fully implemented with create user functionality

--- Assets/Scripts/AccountManager.cs
+++ Assets/Scripts/AccountManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
@@ -14,17 +15,24 @@ public class AccountManager : MonoBehaviour
 
     public static AccountManager instance;
 
-    public Identity userIdentity = null;
+    public User player = null;
+
     public Identity adminIdentity = null;
 
     private readonly string PLATFORM_URL = "https://kovan.cloud.enjin.io/";
     
     [SerializeField]
     private int APP_ID;
+
     [SerializeField]
     private string APP_SECRET;
 
     public GameObject username;
+
+    public GameObject userId;
+
+    public Text createAccInfoText;
+    public Text loginAccInfoText;
 
     public int coinBalance;
 
@@ -37,6 +45,8 @@ public class AccountManager : MonoBehaviour
             instance = this;
         }
 
+        createAccInfoText.text = "";
+        loginAccInfoText.text = "";
         Enjin.SDK.Core.Enjin.StartPlatform(PLATFORM_URL, APP_ID, APP_SECRET);
         LoginAdmin();
     }
@@ -53,56 +63,57 @@ public class AccountManager : MonoBehaviour
     }
 
     async public void Login(){
-        string name = username.GetComponent<InputField>().text;
+        loginAccInfoText.text = "";
+        int id = Int32.Parse(userId.GetComponent<InputField>().text);
 
-        userIdentity = Enjin.SDK.Core.Enjin.GetIdentity(10160);
+        Debug.Log(id);
 
-        // Debug.Log(userIdentity.id);
-        // Debug.Log(userIdentity.wallet.ethAddress);
-        // Debug.Log(userIdentity.user.id);
+        try {
+            player = Enjin.SDK.Core.Enjin.GetUser(id);
 
-        // User player = Enjin.SDK.Core.Enjin.GetUser(name);
+        } catch {
 
-        // Debug.Log("player id: " + player.id);
-        // Debug.Log("player name: " + player.name);
-
-        // User user = Enjin.SDK.Core.Enjin.GetUser(19781);
-
-        User user2 = Enjin.SDK.Core.Enjin.GetUser(name);
-
-        Debug.Log("user id: " + user2.id);
-        Debug.Log("user name: " + user2.name);
-
-        User user = Enjin.SDK.Core.Enjin.GetUser(19781);
-
-        Debug.Log("user length : " + user2.identities.Length);
-
-        for(int i = 0; i < user.identities.Length; i++ ){
-            Debug.Log("Identity id: " + user.identities[i].id);
-            Debug.Log("App id: " + user.identities[i].app.id);
-            Debug.Log("wallet address: " + user.identities[i].wallet.ethAddress);
-            Debug.Log("User name: " + user.name);
+            loginAccInfoText.text = "The user id does not exist.";
+            throw new Exception("There is no account tied to this user id");
         }
 
+        if(player.identities.Length == 0) {
+            loginAccInfoText.text = "The user id in invalid.";
+            throw new Exception("There is no account tied to this user id");
+        }
 
+        Debug.Log(player.identities.Length);
+        Debug.Log(player.identities[0].linkingCode);
+        
+        if(player.identities[0].linkingCode != ""){
+            loginAccInfoText.text = "The user id has not been activated. Please link the account to your wallet address with Linking Code " + player.identities[0].linkingCode +".";
+        } else {
+             AuthPlayer(player.name);
+        }
+  
 
-        // AuthPlayer(name);
+       
     }
 
 
     async public void CreatePlayer(){
+
+        createAccInfoText.text = "";
         string name = username.GetComponent<InputField>().text;
 
- 
         try {
-        User newUser =  Enjin.SDK.Core.Enjin.CreatePlayer(name);
+        player =  Enjin.SDK.Core.Enjin.CreatePlayer(name);
 
-        Debug.Log("The new User ID: " + newUser.id);
-        Debug.Log("The new username: " + newUser.name);
+        Debug.Log("The new User ID: " + player.id);
+        Debug.Log("The new username: " + player.name);
+
 
         } catch {
-            Debug.Log("User already exists, please try a different username");
+
+            throw new Exception("User already exists, please try a different username");
         }
+
+        createAccInfoText.text = "Account Created! Your Login ID is " + player.id +". " + "Use linking code " + player.identities[0].linkingCode + " to link your wallet address to this id. If you forget your User ID, please contact the game creator.";
 
     }
 
@@ -112,7 +123,6 @@ public class AccountManager : MonoBehaviour
         string accessToken = Enjin.SDK.Core.Enjin.AuthPlayer(name);
 
         Debug.Log(accessToken);
-
 
 
         GoToGame();
